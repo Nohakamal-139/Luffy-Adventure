57df134b5b5e9a77fc4f546509bf1f3df90e6aa7
Nohakamal-139 <8251962@horus.edu.eg>
2026-06-28 06:45:40 +0300

    s

--- OnePieceAdventure/OnePieceAdventure/New Piskel (10).tsx
+++ OnePieceAdventure/OnePieceAdventure/New Piskel (10).tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="New Piskel (10)" tilewidth="32" tileheight="32" tilecount="4" columns="2">
  <image source="New Piskel (10).png" width="64" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
